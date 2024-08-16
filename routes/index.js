5c1a182cb52f171f90482ed65e494ce2ab977acb
chrisbmx20720 <christian.al.ac20@gmail.com>
2024-08-16 01:23:30 -0600

    qr app

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,26 @@
 const express = require('express');
-const serverless = require('serverless-http');
-const path = require('path');
-const bodyParser = require('express').urlencoded({ extended: false });
-const indexRouter = require('../routes/index');
+const router = express.Router();
+const qr = require('qrcode');
 
-const app = express();
-
-// Configuración para servir vistas directamente desde la carpeta views
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
-
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Middleware para manejar formularios
-app.use(bodyParser);
-
-// Middleware para manejar las rutas
-app.use('/', indexRouter);
-
-// Manejo de errores
-app.use((req, res) => {
-  res.status(404).send('Página no encontrada');
+// Ruta principal
+router.get('/', (req, res) => {
+  res.render('index', { qrCodeImage: null });
 });
 
-// Exportar como función serverless
-module.exports.handler = serverless(app);
+// Ruta para generar el código QR
+router.post('/', async (req, res) => {
+  const url = req.body.url;
+  if (!url) {
+    return res.render('index', { qrCodeImage: null });
+  }
+
+  try {
+    const qrCodeImage = await qr.toDataURL(url);
+    res.render('index', { qrCodeImage });
+  } catch (err) {
+    console.error(err);
+    res.send('Error occurred');
+  }
+});
+
+module.exports = router;
